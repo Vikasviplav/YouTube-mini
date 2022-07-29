38d97a213ce598e5282de1e03a7f2b7366f5cf56
vikasviplav <vikashvipplav@gmail.com>
2022-07-29 23:32:20 +0530

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let API_KEY = "AIzaSyDXTi3OeS3E1rwtfWfx6vxafPtaifJqzZs"
-let maxlimit = 20
+let maxlimit = 50
 
 
 let displayVideo = (videoList) => {
@@ -9,15 +9,15 @@ let displayVideo = (videoList) => {
   container.innerHTML = ""
   videoList.forEach(video => {
 
-   const {
-    id: { videoId },
-    snippet: {
-      title, channelTitle, thumbnails: {
-        medium : { url },
+    const {
+      id: { videoId },
+      snippet: {
+        title, channelTitle, thumbnails: {
+          medium: { url },
+        },
       },
-    },
-   } = video
-   
+    } = video
+
     let div = document.createElement("div")
 
     let videoThumbnail = document.createElement("img")
@@ -29,19 +29,38 @@ let displayVideo = (videoList) => {
     let videoTitle = document.createElement("p")
     videoTitle.textContent = title
 
-    div.append(videoThumbnail,channelTitle,videoTitle)
-    
-    
+    div.append(videoThumbnail, channelTitle, videoTitle)
+
+
     container.append(div)
 
     div.addEventListener("click", function () {
       var videoId = video.id.videoId;
       localStorage.setItem("videoId", JSON.stringify(videoId))
       window.open("./individualVideo.html", '_blank').focus();
-  })
+    })
 
   });
 }
+
+
+var autoCall = async ()=> {
+
+  try {
+    let search = "most popular video in india"
+    let url = `https://youtube.googleapis.com/youtube/v3/search?key=${API_KEY}&q=${search}&maxResults=${maxlimit}&type=video&part=snippet`
+
+    var res = await fetch(url);
+
+    var data = await res.json();
+
+    displayVideo(data.items)
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+autoCall()
 
 
 
